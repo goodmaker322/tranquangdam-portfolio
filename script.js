08142ea4d82d040c1a872186f9fca3f3c3300ff3
goodmaker322 <93530396+goodmaker322@users.noreply.github.com>
2026-04-08 10:27:46 +0700

    update code

---
+++ script.js
@@ -0,0 +1,20 @@
+// CURSOR
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
+// REVEAL
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+});
+
+document.querySelectorAll(".section").forEach((el) => {
+  observer.observe(el);
+});
